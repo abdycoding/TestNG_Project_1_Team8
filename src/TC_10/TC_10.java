8d437675b4df1b9d14f1d9b0d045a695436e10ca
yaseyldrr <116019129+yaseyldrr@users.noreply.github.com>
2024-10-09 20:02:49 +0300

    TC_10 code is created.

--- src/TC_10/TC_10.java
+++ src/TC_10/TC_10.java
@@ -1,4 +1,31 @@
 package TC_10;
 
-public class TC_10 {
+import Utlity.BaseDriverGenel;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_10 extends BaseDriverGenel {
+    @Test
+    public void Test10() {
+        driver.get("https://demo.openmrs.org/openmrs/login.htm");
+        driver.manage().window().maximize();
+
+        TC_10Elements tc = new TC_10Elements();
+
+        tc.username.sendKeys("Admin");
+        tc.password.sendKeys("Admin123");
+        tc.inpatientWard.click();
+        tc.loginButton.click();
+        tc.appShcButton.click();
+        tc.mngAppointmentButton.click();
+        tc.patientSearch.sendKeys("100J27");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='patient-search-results-table']/tbody/tr/td[1]")));
+        tc.getPatientSearch.click();
+        wait.until(ExpectedConditions.visibilityOf(tc.getAssert));
+        Assert.assertTrue(tc.getAssert.getText().contains("Your computer is not set to the right time zone. Please change to Coordinated Universal Time" +
+                " and then close and restart your browser to assure proper scheduling functionality."),"Failed");
+
+    }
 }
