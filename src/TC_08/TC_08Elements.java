8ea78c2cb235ad55bd3d7a7bffb0e780a8b3f022
furkanakalin <furkanakalin3423@gmail.com>
2024-10-10 14:44:24 +0300

    TC_08 has been complated, but need to update.

--- src/TC_08/TC_08Elements.java
+++ src/TC_08/TC_08Elements.java
@@ -1,4 +1,40 @@
 package TC_08;
 
+import Utlity.BaseDriverGenel;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
 public class TC_08Elements {
+
+    public TC_08Elements() {
+
+        PageFactory.initElements(BaseDriverGenel.driver, this);
+    }
+
+    @FindBy(xpath = "//input[@id='username']")
+    public WebElement userNameBox;
+
+    @FindBy(xpath = "//input[@id='password']")
+    public WebElement passwordBox;
+
+    @FindBy(xpath = "//li[@id='Registration Desk']")
+    public WebElement registrationDeskButton;
+
+    @FindBy(xpath = "//input[@type='submit']")
+    public WebElement loginButton;
+
+    @FindBy(xpath = "//i[@class='icon-search']")
+    public WebElement findPatientButton;
+
+    @FindBy(xpath = "//*[@id='patient-search-results-table_info']")
+    public WebElement patientcountinfo;
+
+    @FindBy(css = "[role='alert']>tr")
+    public List<WebElement> rowcounts;
+
+
+
 }
