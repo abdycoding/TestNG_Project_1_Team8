5bfffc7e8d3df81a698d7385cbae39cebe6cb3a6
yaseyldrr <116019129+yaseyldrr@users.noreply.github.com>
2024-10-11 20:46:34 +0300

    TC_05 code is created.

--- src/TC_05/TC_05.java
+++ src/TC_05/TC_05.java
@@ -1,4 +1,40 @@
 package TC_05;
 
-public class TC_05 {
+import TC_10.TC_10Elements;
+import Utlity.BaseDriverGenel;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_05 extends BaseDriverGenel {
+    @Test
+    public void Test10() {
+        driver.get("https://demo.openmrs.org/openmrs/login.htm");
+        driver.manage().window().maximize();
+
+        TC_05Elements tc = new TC_05Elements();
+
+        tc.username.sendKeys("Admin");
+        tc.password.sendKeys("Admin123");
+        tc.inpatientWard.click();
+        tc.loginButton.click();
+        Actions actions = new Actions(BaseDriverGenel.driver);
+        Action action = actions.moveToElement(tc.icon).build();
+        action.perform();
+        wait.until(ExpectedConditions.visibilityOf(tc.iconLink));
+        Assert.assertTrue(tc.iconLink.getText().equals("My Account"), "Failed");
+        tc.iconLink.click();
+
+        WebElement clickableElement1 = wait.until(ExpectedConditions.elementToBeClickable(tc.changePassword));
+        Assert.assertTrue(clickableElement1.isDisplayed() && clickableElement1.isEnabled() &&
+                tc.changePassword.getText().contains("Change Password"));
+
+        WebElement clickableElement2 = wait.until(ExpectedConditions.elementToBeClickable(tc.myLanguages));
+        Assert.assertTrue(clickableElement2.isDisplayed() && clickableElement2.isEnabled() &&
+                tc.myLanguages.getText().contains("My Languages"));
+
+    }
 }
--- src/TC_05/TC_05Elements.java
+++ src/TC_05/TC_05Elements.java
@@ -1,4 +1,40 @@
 package TC_05;
 
+import Utlity.BaseDriverGenel;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
 public class TC_05Elements {
+
+    public TC_05Elements() {
+        PageFactory.initElements(BaseDriverGenel.driver, this);
+    }
+
+    @FindBy(id = "username")
+    public WebElement username;
+
+    @FindBy(id = "password")
+    public WebElement password;
+
+    @FindBy(id = "Inpatient Ward")
+    public WebElement inpatientWard;
+
+    @FindBy(id = "loginButton")
+    public WebElement loginButton;
+
+    @FindBy(xpath = "//*[@id='navbarSupportedContent']/ul/li[1]/i[1]")
+    public WebElement icon;
+
+    @FindBy(xpath = "//*[@id='user-account-menu']/li/a")
+    public WebElement iconLink;
+
+    @FindBy(xpath = "//*[@id='tasks']/a[1]/div")
+    public WebElement changePassword;
+
+    @FindBy(xpath = "//*[@id='tasks']/a[2]/div")
+    public WebElement myLanguages;
+
 }
